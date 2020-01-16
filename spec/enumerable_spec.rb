31cfc37ed0e6ad6278e06e7880e9b8757ba7a763
Wesley Wang <wesleywang2@gmail.com>
2020-01-15 20:08:05 -0800

    Add spec file for enumerable

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,8 +3,15 @@ require "./enumerable_methods.rb"
 RSpec.describe Enumerable do
   describe "#my_select" do
     it "returns a filtered list of only even numbers" do
-      expect([1,2,3].select {|n| n.even?}).to eql([2])
-      # expect(Enumerable.my_select([1,2,3,4,5,6])).to eql([2,4,6])
+      expect([1,2,3].my_select {|n| n.even?}).to eql([2])
+    end
+    it "returns an empty array when an empty array is given" do
+      expect([].my_select {|n| n.even?}).to eql([])
+    end
+    it "returns words without the letter 'a'" do
+      expect(["apple", "carrot", "kiwi", "banana", "celery"].my_select do |word|
+        word.include?("a")
+      end).to eql(["apple", "carrot", "banana"])
     end
   end
 end
